b74a2d5851eedcd36600ad5592a9275b5193a9fe
asyks <aaron@aaronsykes.com>
2018-11-16 16:04:53 -0800

    Async egress queue handling

--- sqs_cue/config.py
+++ sqs_cue/config.py
@@ -7,6 +7,4 @@ with open(constants.QUEUE_CONFIG_FILE, 'r') as config_file:
     queue_config = yaml.load(config_file)
 
 ingress_queue = queue_config['ingress_queues'][0]
-print(ingress_queue)
 egress_queues = queue_config['egress_queues']
-print(egress_queues)
--- sqs_cue/main.py
+++ sqs_cue/main.py
@@ -1,11 +1,28 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
+
+import asyncio
 
 import config
 import log
 import sqs_client
 
 
-def log_poll_queue():
+async def print_message(queue, message):
+    message_id = message['MessageId']
+    queue_url = queue['queue_url']
+    print(f'Sending message {message_id} to queue {queue_url}')
+
+    return True
+
+
+async def async_send_message(queues, message):
+    statuses = await asyncio.gather(
+        *(print_message(queue, message) for queue in queues)
+    )
+    print(statuses)
+
+
+def long_poll_queue():
     client = sqs_client.SqsClient(
         access_key_id=config.ingress_queue['access_key_id'],
         access_key=config.ingress_queue['access_key'],
@@ -25,15 +42,19 @@ def log_poll_queue():
             break
         else:
             if message:
+                asyncio.run(
+                    async_send_message(
+                        config.egress_queues, message
+                    )
+                )
+
                 client.delete_message(
                     config.ingress_queue['queue_url'], message
                 )
 
-    # TODO: send each message asynchronously to secondary queues
-
 
 def main():
-    log_poll_queue()
+    long_poll_queue()
 
 
 if __name__ == '__main__':
