6577d36d5dae82b50b1035a4444b1cc098c39483
asyks <aaron@aaronsykes.com>
2018-11-19 15:47:44 -0800

    Refactor main codepath

--- sqs_cue/main.py
+++ sqs_cue/main.py
@@ -1,48 +1,77 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python
 
 import asyncio
+import logging
 
 import config
 import log
-import sqs_client
+import sqs
+
+
+logger = logging.getLogger(__name__)
+
+
+async def transform_message(transformer, message):
+    if not transformer:
+        return message
+    else:
+        return message
+
+    # Retrieve transformer, execute, return result
 
 
 async def handle_route(route, message):
-    message_id = message['MessageId']
-
-    if route['type'] == 'sqs':
-        try:
-            queue_url = route['queue_url']
-        except KeyError:
-            raise KeyError("route is type 'sqs', but 'queue_url' not provided")
-
-        try:
-            access_key_id = route['access_key_id']
-            access_key = route['access_key']
-            region = route['region']
-        except KeyError:
-            raise KeyError(
-                "route with queue_url {queue_url} is misconfigured, "
-                "must provide: access_key_id, access_key, and region"
-            )
-
-        print(f'Sending message {message_id} to queue {queue_url}')
-        client = sqs_client.SqsClient(
-            access_key_id=access_key_id, access_key=access_key, region=region
-        )
-        response = client.enqueue(route['queue_url'], 'type1', message['Body'])
-
-    if route['type'] == 'raw':
-        print(f'Sending message {message_id} as request')
+    failure = False
 
     try:
-        status_code = response['ResponseMetadata']['HTTPStatusCode']
+        message_id = message['MessageId']
     except KeyError:
-        status_code = None
-
-    failure = False
-    if route['critical'] and status_code != 200:
+        logger.error("Received message did not contain key 'MessageId'")
         failure = True
+
+    else:
+        if route['type'] == 'sqs':
+            try:
+                queue_url = route['url']
+            except KeyError:
+                raise KeyError(
+                    "route is type 'sqs', but queue 'url' not provided"
+                )
+
+            try:
+                access_key_id = route['access_key_id']
+                access_key = route['access_key']
+                region = route['region']
+            except KeyError:
+                raise KeyError(
+                    "route with queue_url {queue_url} is misconfigured, "
+                    "must provide: access_key_id, access_key, and region"
+                )
+
+            logger.info(
+                'Sending message %s to queue %s', message_id, queue_url
+            )
+
+            client = sqs.Client(
+                access_key_id=access_key_id,
+                access_key=access_key,
+                region=region
+            )
+            response = client.enqueue(route['url'], 'type1', message['Body'])
+
+        if route['type'] == 'raw_http':
+            logger.info(
+                'Sending message %s as raw http request', message_id
+            )
+            response = {}
+
+        try:
+            status_code = response['ResponseMetadata']['HTTPStatusCode']
+        except KeyError:
+            status_code = None
+
+        if not failure and route['critical'] and status_code != 200:
+            failure = True
 
     return failure
 
@@ -61,36 +90,53 @@ async def async_process_message(routes, message):
 
 
 def long_poll_queue():
-    client = sqs_client.SqsClient(
-        access_key_id=config.receiver['access_key_id'],
-        access_key=config.receiver['access_key'],
-        region=config.receiver['region'],
-    )
+    try:
+        queue_url = config.receiver['url']
+    except KeyError:
+        raise KeyError("receiver queue 'url' not provided")
 
-    # Retrieve messages from receiver queue
-    dequeue = client.dequeue(config.receiver['queue_url'])
+    try:
+        access_key_id = config.receiver['access_key_id']
+        access_key = config.receiver['access_key']
+        region = config.receiver['region']
+    except KeyError:
+        raise KeyError(
+            "receiver with queue_url {queue_url} is misconfigured, "
+            "must provide: access_key_id, access_key, and region"
+        )
+
+    # Instantiate Long Poller on receiver queue
+    client = sqs.Client(
+        access_key_id=access_key_id,
+        access_key=access_key,
+        region=region
+    )
+    dequeue = client.dequeue(queue_url)
+
     while True:
         try:
             message = next(dequeue)
         except StopIteration:
-            print('Stopping Long Polling due to StopIteration')
-            break
+            raise StopIteration('Stopping Long Polling due to StopIteration')
         except KeyboardInterrupt:
-            print('Stopping Long Polling due to KeyboardInterrupt')
-            break
+            raise KeyboardInterrupt(
+                'Stopping Long Polling due to KeyboardInterrupt'
+            )
         else:
             if message:
+                # Send message to each route asynchronously
                 success = asyncio.run(
                     async_process_message(config.routes, message)
                 )
 
-                message_id = message['MessageId']
                 if success:
                     client.delete_message(
-                        config.receiver['queue_url'], message
+                        config.receiver['url'], message
                     )
                 else:
-                    print(f'Message {message_id} handling failed.')
+                    logger.warning(
+                        'Message %s handling failed.', message['MessageId'],
+                    )
 
 
 def main():
